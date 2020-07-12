8cfd811381371533aa3dc3ec0b50c6de4e601c20
spacegoing <spacegoing@gmail.com>
2020-07-13 00:05:47 +1000

    [New] format code

--- deep_rl/agent/ASquaredC_A2C_agent.py
+++ deep_rl/agent/ASquaredC_A2C_agent.py
@@ -10,168 +10,194 @@ from .BaseAgent import *
 
 
 class ASquaredCA2CAgent(BaseAgent):
-    def __init__(self, config):
-        BaseAgent.__init__(self, config)
-        self.config = config
-        self.task = config.task_fn()
-        self.network = config.network_fn()
-        self.opt = config.optimizer_fn(self.network.parameters())
-        self.total_steps = 0
 
-        self.worker_index = tensor(np.arange(config.num_workers)).long()
-        self.states = self.task.reset()
-        self.states = config.state_normalizer(self.states)
-        self.is_initial_states = tensor(np.ones((config.num_workers))).byte()
-        self.prev_options = tensor(np.zeros(config.num_workers)).long()
+  def __init__(self, config):
+    BaseAgent.__init__(self, config)
+    self.config = config
+    self.task = config.task_fn()
+    self.network = config.network_fn()
+    self.opt = config.optimizer_fn(self.network.parameters())
+    self.total_steps = 0
 
-        self.count = 0
+    self.worker_index = tensor(np.arange(config.num_workers)).long()
+    self.states = self.task.reset()
+    self.states = config.state_normalizer(self.states)
+    self.is_initial_states = tensor(np.ones((config.num_workers))).byte()
+    self.prev_options = tensor(np.zeros(config.num_workers)).long()
 
-    def compute_pi_hat(self, prediction, prev_option, is_intial_states):
-        inter_pi = prediction['inter_pi']
-        mask = torch.zeros_like(inter_pi)
-        mask[self.worker_index, prev_option] = 1
-        beta = prediction['beta']
-        pi_hat = (1 - beta) * mask + beta * inter_pi
-        is_intial_states = is_intial_states.view(-1, 1).expand(-1, inter_pi.size(1))
-        pi_hat = torch.where(is_intial_states, inter_pi, pi_hat)
-        return pi_hat
+    self.count = 0
 
-    def compute_pi_bar(self, options, action, mean, std):
-        options = options.unsqueeze(-1).expand(-1, -1, mean.size(-1))
-        mean = mean.gather(1, options).squeeze(1)
-        std = std.gather(1, options).squeeze(1)
-        dist = torch.distributions.Normal(mean, std)
-        pi_bar = dist.log_prob(action).sum(-1).exp().unsqueeze(-1)
-        return pi_bar
+  def compute_pi_hat(self, prediction, prev_option, is_intial_states):
+    inter_pi = prediction['inter_pi']
+    mask = torch.zeros_like(inter_pi)
+    mask[self.worker_index, prev_option] = 1
+    beta = prediction['beta']
+    pi_hat = (1 - beta) * mask + beta * inter_pi
+    is_intial_states = is_intial_states.view(-1, 1).expand(-1, inter_pi.size(1))
+    pi_hat = torch.where(is_intial_states, inter_pi, pi_hat)
+    return pi_hat
 
-    def compute_log_pi_a(self, options, pi_hat, action, mean, std, mdp):
-        if mdp == 'hat':
-            return pi_hat.add(1e-5).log().gather(1, options)
-        elif mdp == 'bar':
-            pi_bar = self.compute_pi_bar(options, action, mean, std)
-            return pi_bar.add(1e-5).log()
-        else:
-            raise NotImplementedError
+  def compute_pi_bar(self, options, action, mean, std):
+    options = options.unsqueeze(-1).expand(-1, -1, mean.size(-1))
+    mean = mean.gather(1, options).squeeze(1)
+    std = std.gather(1, options).squeeze(1)
+    dist = torch.distributions.Normal(mean, std)
+    pi_bar = dist.log_prob(action).sum(-1).exp().unsqueeze(-1)
+    return pi_bar
 
-    def compute_adv(self, storage, mdp):
-        config = self.config
+  def compute_log_pi_a(self, options, pi_hat, action, mean, std, mdp):
+    if mdp == 'hat':
+      return pi_hat.add(1e-5).log().gather(1, options)
+    elif mdp == 'bar':
+      pi_bar = self.compute_pi_bar(options, action, mean, std)
+      return pi_bar.add(1e-5).log()
+    else:
+      raise NotImplementedError
 
-        v = storage.__getattribute__('v_%s' % (mdp))
-        adv = storage.__getattribute__('adv_%s' % (mdp))
-        all_ret = storage.__getattribute__('ret_%s' % (mdp))
+  def compute_adv(self, storage, mdp):
+    config = self.config
 
-        ret = v[-1].detach()
-        advantages = tensor(np.zeros((config.num_workers, 1)))
-        for i in reversed(range(config.rollout_length)):
-            ret = storage.r[i] + config.discount * storage.m[i] * ret
-            if not config.use_gae:
-                advantages = ret - v[i].detach()
-            else:
-                td_error = storage.r[i] + config.discount * storage.m[i] * v[i + 1] - v[i]
-                advantages = advantages * config.gae_tau * config.discount * storage.m[i] + td_error
-            adv[i] = advantages.detach()
-            all_ret[i] = ret.detach()
+    v = storage.__getattribute__('v_%s' % (mdp))
+    adv = storage.__getattribute__('adv_%s' % (mdp))
+    all_ret = storage.__getattribute__('ret_%s' % (mdp))
 
-    def learn(self, storage, mdp, freeze_v=False):
-        config = self.config
-        log_prob, v, ret, adv, pi_hat = storage.cat(['log_pi_%s' % (mdp), 'v_%s' % (mdp), 'ret_%s' % (mdp), 'adv_%s' % (mdp), 'pi_hat'])
+    ret = v[-1].detach()
+    advantages = tensor(np.zeros((config.num_workers, 1)))
+    for i in reversed(range(config.rollout_length)):
+      ret = storage.r[i] + config.discount * storage.m[i] * ret
+      if not config.use_gae:
+        advantages = ret - v[i].detach()
+      else:
+        td_error = storage.r[i] + config.discount * storage.m[i] * v[i +
+                                                                     1] - v[i]
+        advantages = advantages * config.gae_tau * config.discount * storage.m[
+            i] + td_error
+      adv[i] = advantages.detach()
+      all_ret[i] = ret.detach()
 
-        if mdp == 'hat':
-            entropy = -(pi_hat * pi_hat.add(1e-5).log()).sum(-1).mean()
-        elif mdp == 'bar':
-            entropy = 0
-        else:
-            raise NotImplementedError
+  def learn(self, storage, mdp, freeze_v=False):
+    config = self.config
+    log_prob, v, ret, adv, pi_hat = storage.cat([
+        'log_pi_%s' % (mdp),
+        'v_%s' % (mdp),
+        'ret_%s' % (mdp),
+        'adv_%s' % (mdp), 'pi_hat'
+    ])
 
-        policy_loss = -(log_prob * adv.detach()).mean() - config.entropy_weight * entropy
-        value_loss = (v - ret.detach()).pow(2).mul(0.5).mean()
+    if mdp == 'hat':
+      entropy = -(pi_hat * pi_hat.add(1e-5).log()).sum(-1).mean()
+    elif mdp == 'bar':
+      entropy = 0
+    else:
+      raise NotImplementedError
 
-        if freeze_v:
-            value_loss = 0
+    policy_loss = -(log_prob *
+                    adv.detach()).mean() - config.entropy_weight * entropy
+    value_loss = (v - ret.detach()).pow(2).mul(0.5).mean()
 
-        loss = policy_loss + value_loss
-        return loss
+    if freeze_v:
+      value_loss = 0
 
-    def step(self):
-        config = self.config
-        storage = Storage(config.rollout_length, ['adv_bar', 'adv_hat', 'ret_bar', 'ret_hat'])
-        states = self.states
-        for _ in range(config.rollout_length):
-            prediction = self.network(states)
-            pi_hat = self.compute_pi_hat(prediction, self.prev_options, self.is_initial_states)
-            dist = torch.distributions.Categorical(probs=pi_hat)
-            options = dist.sample()
+    loss = policy_loss + value_loss
+    return loss
 
-            mean = prediction['mean'][self.worker_index, options]
-            std = prediction['std'][self.worker_index, options]
-            dist = torch.distributions.Normal(mean, std)
-            actions = dist.sample()
+  def step(self):
+    config = self.config
+    storage = Storage(config.rollout_length,
+                      ['adv_bar', 'adv_hat', 'ret_bar', 'ret_hat'])
+    states = self.states
+    for _ in range(config.rollout_length):
+      prediction = self.network(states)
+      pi_hat = self.compute_pi_hat(prediction, self.prev_options,
+                                   self.is_initial_states)
+      dist = torch.distributions.Categorical(probs=pi_hat)
+      options = dist.sample()
 
-            pi_bar = self.compute_pi_bar(options.unsqueeze(-1), actions,
-                                         prediction['mean'], prediction['std'])
+      mean = prediction['mean'][self.worker_index, options]
+      std = prediction['std'][self.worker_index, options]
+      dist = torch.distributions.Normal(mean, std)
+      actions = dist.sample()
 
-            v_bar = prediction['q_o'].gather(1, options.unsqueeze(-1))
-            v_hat = (prediction['q_o'] * pi_hat).sum(-1).unsqueeze(-1)
+      pi_bar = self.compute_pi_bar(
+          options.unsqueeze(-1), actions, prediction['mean'], prediction['std'])
 
-            next_states, rewards, terminals, info = self.task.step(to_np(actions))
-            self.record_online_return(info)
-            rewards = config.reward_normalizer(rewards)
-            next_states = config.state_normalizer(next_states)
-            storage.add(prediction)
+      v_bar = prediction['q_o'].gather(1, options.unsqueeze(-1))
+      v_hat = (prediction['q_o'] * pi_hat).sum(-1).unsqueeze(-1)
 
-            storage.add({'r': tensor(rewards).unsqueeze(-1),
-                         'm': tensor(1 - terminals).unsqueeze(-1),
-                         'a': actions,
-                         'o': options.unsqueeze(-1),
-                         'prev_o': self.prev_options.unsqueeze(-1),
-                         's': tensor(states),
-                         'init': self.is_initial_states.unsqueeze(-1),
-                         'pi_hat': pi_hat,
-                         'log_pi_hat': pi_hat[self.worker_index, options].add(1e-5).log().unsqueeze(-1),
-                         'log_pi_bar': pi_bar.add(1e-5).log(),
-                         'v_bar': v_bar,
-                         'v_hat': v_hat})
+      next_states, rewards, terminals, info = self.task.step(to_np(actions))
+      self.record_online_return(info)
+      rewards = config.reward_normalizer(rewards)
+      next_states = config.state_normalizer(next_states)
+      storage.add(prediction)
 
-            self.is_initial_states = tensor(terminals).byte()
-            self.prev_options = options
+      storage.add({
+          'r':
+              tensor(rewards).unsqueeze(-1),
+          'm':
+              tensor(1 - terminals).unsqueeze(-1),
+          'a':
+              actions,
+          'o':
+              options.unsqueeze(-1),
+          'prev_o':
+              self.prev_options.unsqueeze(-1),
+          's':
+              tensor(states),
+          'init':
+              self.is_initial_states.unsqueeze(-1),
+          'pi_hat':
+              pi_hat,
+          'log_pi_hat':
+              pi_hat[self.worker_index, options].add(1e-5).log().unsqueeze(-1),
+          'log_pi_bar':
+              pi_bar.add(1e-5).log(),
+          'v_bar':
+              v_bar,
+          'v_hat':
+              v_hat
+      })
 
-            states = next_states
-            self.total_steps += config.num_workers
+      self.is_initial_states = tensor(terminals).byte()
+      self.prev_options = options
 
-        self.states = states
-        prediction = self.network(states)
-        pi_hat = self.compute_pi_hat(prediction, self.prev_options, self.is_initial_states)
-        dist = torch.distributions.Categorical(pi_hat)
-        options = dist.sample()
-        v_bar = prediction['q_o'].gather(1, options.unsqueeze(-1))
-        v_hat = (prediction['q_o'] * pi_hat).sum(-1).unsqueeze(-1)
+      states = next_states
+      self.total_steps += config.num_workers
 
-        storage.add(prediction)
-        storage.add({
-            'v_bar': v_bar,
-            'v_hat': v_hat,
-        })
-        storage.placeholder()
+    self.states = states
+    prediction = self.network(states)
+    pi_hat = self.compute_pi_hat(prediction, self.prev_options,
+                                 self.is_initial_states)
+    dist = torch.distributions.Categorical(pi_hat)
+    options = dist.sample()
+    v_bar = prediction['q_o'].gather(1, options.unsqueeze(-1))
+    v_hat = (prediction['q_o'] * pi_hat).sum(-1).unsqueeze(-1)
 
-        self.compute_adv(storage, 'bar')
-        self.compute_adv(storage, 'hat')
+    storage.add(prediction)
+    storage.add({
+        'v_bar': v_bar,
+        'v_hat': v_hat,
+    })
+    storage.placeholder()
 
-        if config.learning == 'all':
-            mdps = ['hat', 'bar']
-            np.random.shuffle(mdps)
-            loss1 = self.learn(storage, mdps[0])
-            loss2 = self.learn(storage, mdps[1], freeze_v=config.freeze_v)
-            loss = loss1 + loss2
-        elif config.learning == 'alt':
-            if self.count % 2:
-                loss = self.learn(storage, 'hat')
-            else:
-                loss = self.learn(storage, 'bar')
-            self.count += 1
-        else:
-            raise NotImplementedError
+    self.compute_adv(storage, 'bar')
+    self.compute_adv(storage, 'hat')
 
-        self.opt.zero_grad()
-        loss.backward()
-        nn.utils.clip_grad_norm_(self.network.parameters(), config.gradient_clip)
-        self.opt.step()
+    if config.learning == 'all':
+      mdps = ['hat', 'bar']
+      np.random.shuffle(mdps)
+      loss1 = self.learn(storage, mdps[0])
+      loss2 = self.learn(storage, mdps[1], freeze_v=config.freeze_v)
+      loss = loss1 + loss2
+    elif config.learning == 'alt':
+      if self.count % 2:
+        loss = self.learn(storage, 'hat')
+      else:
+        loss = self.learn(storage, 'bar')
+      self.count += 1
+    else:
+      raise NotImplementedError
+
+    self.opt.zero_grad()
+    loss.backward()
+    nn.utils.clip_grad_norm_(self.network.parameters(), config.gradient_clip)
+    self.opt.step()
--- deep_rl/agent/BaseAgent.py
+++ deep_rl/agent/BaseAgent.py
@@ -13,169 +13,175 @@ from skimage.io import imsave
 
 
 class BaseAgent:
-    def __init__(self, config):
-        self.config = config
-        self.logger = get_logger(tag=config.tag, log_level=config.log_level)
-        self.task_ind = 0
 
-    def close(self):
-        close_obj(self.task)
+  def __init__(self, config):
+    self.config = config
+    self.logger = get_logger(tag=config.tag, log_level=config.log_level)
+    self.task_ind = 0
 
-    def save(self, filename):
-        torch.save(self.network.state_dict(), '%s.model' % (filename))
-        with open('%s.stats' % (filename), 'wb') as f:
-            pickle.dump(self.config.state_normalizer.state_dict(), f)
+  def close(self):
+    close_obj(self.task)
 
-    def load(self, filename):
-        state_dict = torch.load('%s.model' % filename, map_location=lambda storage, loc: storage)
-        self.network.load_state_dict(state_dict)
-        with open('%s.stats' % (filename), 'rb') as f:
-            self.config.state_normalizer.load_state_dict(pickle.load(f))
+  def save(self, filename):
+    torch.save(self.network.state_dict(), '%s.model' % (filename))
+    with open('%s.stats' % (filename), 'wb') as f:
+      pickle.dump(self.config.state_normalizer.state_dict(), f)
 
-    def eval_step(self, state):
-        raise NotImplementedError
+  def load(self, filename):
+    state_dict = torch.load(
+        '%s.model' % filename, map_location=lambda storage, loc: storage)
+    self.network.load_state_dict(state_dict)
+    with open('%s.stats' % (filename), 'rb') as f:
+      self.config.state_normalizer.load_state_dict(pickle.load(f))
 
-    def eval_episode(self):
-        env = self.config.eval_env
-        state = env.reset()
-        while True:
-            action = self.eval_step(state)
-            state, reward, done, info = env.step(action)
-            ret = info[0]['episodic_return']
-            if ret is not None:
-                break
-        return ret
+  def eval_step(self, state):
+    raise NotImplementedError
 
-    def eval_episodes(self):
-        episodic_returns = []
-        for ep in range(self.config.eval_episodes):
-            total_rewards = self.eval_episode()
-            episodic_returns.append(np.sum(total_rewards))
-        self.logger.info('steps %d, episodic_return_test %.2f(%.2f)' % (
-            self.total_steps, np.mean(episodic_returns), np.std(episodic_returns) / np.sqrt(len(episodic_returns))
-        ))
-        self.logger.add_scalar('episodic_return_test', np.mean(episodic_returns), self.total_steps)
-        return {
-            'episodic_return_test': np.mean(episodic_returns),
-        }
+  def eval_episode(self):
+    env = self.config.eval_env
+    state = env.reset()
+    while True:
+      action = self.eval_step(state)
+      state, reward, done, info = env.step(action)
+      ret = info[0]['episodic_return']
+      if ret is not None:
+        break
+    return ret
 
-    def record_online_return(self, info, offset=0):
-        if isinstance(info, dict):
-            ret = info['episodic_return']
-            if ret is not None:
-                self.logger.add_scalar('episodic_return_train', ret, self.total_steps + offset)
-                self.logger.info('steps %d, episodic_return_train %s' % (self.total_steps + offset, ret))
-        elif isinstance(info, tuple):
-            for i, info_ in enumerate(info):
-                self.record_online_return(info_, i)
-        else:
-            raise NotImplementedError
+  def eval_episodes(self):
+    episodic_returns = []
+    for ep in range(self.config.eval_episodes):
+      total_rewards = self.eval_episode()
+      episodic_returns.append(np.sum(total_rewards))
+    self.logger.info(
+        'steps %d, episodic_return_test %.2f(%.2f)' %
+        (self.total_steps, np.mean(episodic_returns),
+         np.std(episodic_returns) / np.sqrt(len(episodic_returns))))
+    self.logger.add_scalar('episodic_return_test', np.mean(episodic_returns),
+                           self.total_steps)
+    return {
+        'episodic_return_test': np.mean(episodic_returns),
+    }
 
-    def switch_task(self):
-        config = self.config
-        if not config.tasks:
-            return
-        segs = np.linspace(0, config.max_steps, len(config.tasks) + 1)
-        if self.total_steps > segs[self.task_ind + 1]:
-            self.task_ind += 1
-            self.task = config.tasks[self.task_ind]
-            self.states = self.task.reset()
-            self.states = config.state_normalizer(self.states)
+  def record_online_return(self, info, offset=0):
+    if isinstance(info, dict):
+      ret = info['episodic_return']
+      if ret is not None:
+        self.logger.add_scalar('episodic_return_train', ret,
+                               self.total_steps + offset)
+        self.logger.info('steps %d, episodic_return_train %s' %
+                         (self.total_steps + offset, ret))
+    elif isinstance(info, tuple):
+      for i, info_ in enumerate(info):
+        self.record_online_return(info_, i)
+    else:
+      raise NotImplementedError
 
-    def record_episode(self, dir, env):
-        mkdir(dir)
-        steps = 0
-        state = env.reset()
-        while True:
-            action = self.record_step(state)
-            self.record_obs(env, dir, steps)
-            state, reward, done, info = env.step(action)
-            ret = info[0]['episodic_return']
-            steps += 1
-            if ret is not None:
-                break
+  def switch_task(self):
+    config = self.config
+    if not config.tasks:
+      return
+    segs = np.linspace(0, config.max_steps, len(config.tasks) + 1)
+    if self.total_steps > segs[self.task_ind + 1]:
+      self.task_ind += 1
+      self.task = config.tasks[self.task_ind]
+      self.states = self.task.reset()
+      self.states = config.state_normalizer(self.states)
 
-    def record_step(self, state):
-        raise NotImplementedError
+  def record_episode(self, dir, env):
+    mkdir(dir)
+    steps = 0
+    state = env.reset()
+    while True:
+      action = self.record_step(state)
+      self.record_obs(env, dir, steps)
+      state, reward, done, info = env.step(action)
+      ret = info[0]['episodic_return']
+      steps += 1
+      if ret is not None:
+        break
 
-    # For DMControl
-    def record_obs(self, env, dir, steps):
-        env = env.env.envs[0]
-        obs = env.render(mode='rgb_array')
-        imsave('%s/%04d.png' % (dir, steps), obs)
+  def record_step(self, state):
+    raise NotImplementedError
+
+  # For DMControl
+  def record_obs(self, env, dir, steps):
+    env = env.env.envs[0]
+    obs = env.render(mode='rgb_array')
+    imsave('%s/%04d.png' % (dir, steps), obs)
 
 
 class BaseActor(mp.Process):
-    STEP = 0
-    RESET = 1
-    EXIT = 2
-    SPECS = 3
-    NETWORK = 4
-    CACHE = 5
+  STEP = 0
+  RESET = 1
+  EXIT = 2
+  SPECS = 3
+  NETWORK = 4
+  CACHE = 5
 
-    def __init__(self, config):
-        mp.Process.__init__(self)
-        self.config = config
-        self.__pipe, self.__worker_pipe = mp.Pipe()
+  def __init__(self, config):
+    mp.Process.__init__(self)
+    self.config = config
+    self.__pipe, self.__worker_pipe = mp.Pipe()
 
-        self._state = None
-        self._task = None
-        self._network = None
-        self._total_steps = 0
-        self.__cache_len = 2
+    self._state = None
+    self._task = None
+    self._network = None
+    self._total_steps = 0
+    self.__cache_len = 2
 
-        if not config.async_actor:
-            self.start = lambda: None
-            self.step = self._sample
-            self.close = lambda: None
-            self._set_up()
-            self._task = config.task_fn()
+    if not config.async_actor:
+      self.start = lambda: None
+      self.step = self._sample
+      self.close = lambda: None
+      self._set_up()
+      self._task = config.task_fn()
 
-    def _sample(self):
-        transitions = []
-        for _ in range(self.config.sgd_update_frequency):
-            transitions.append(self._transition())
-        return transitions
+  def _sample(self):
+    transitions = []
+    for _ in range(self.config.sgd_update_frequency):
+      transitions.append(self._transition())
+    return transitions
 
-    def run(self):
-        self._set_up()
-        torch.cuda.is_available()
-        config = self.config
-        self._task = config.task_fn()
+  def run(self):
+    self._set_up()
+    torch.cuda.is_available()
+    config = self.config
+    self._task = config.task_fn()
 
-        cache = deque([], maxlen=2)
-        while True:
-            op, data = self.__worker_pipe.recv()
-            if op == self.STEP:
-                if not len(cache):
-                    cache.append(self._sample())
-                    cache.append(self._sample())
-                self.__worker_pipe.send(cache.popleft())
-                cache.append(self._sample())
-            elif op == self.EXIT:
-                self.__worker_pipe.close()
-                return
-            elif op == self.NETWORK:
-                self._network = data
-            else:
-                raise NotImplementedError
-
-    def _transition(self):
+    cache = deque([], maxlen=2)
+    while True:
+      op, data = self.__worker_pipe.recv()
+      if op == self.STEP:
+        if not len(cache):
+          cache.append(self._sample())
+          cache.append(self._sample())
+        self.__worker_pipe.send(cache.popleft())
+        cache.append(self._sample())
+      elif op == self.EXIT:
+        self.__worker_pipe.close()
+        return
+      elif op == self.NETWORK:
+        self._network = data
+      else:
         raise NotImplementedError
 
-    def _set_up(self):
-        pass
+  def _transition(self):
+    raise NotImplementedError
 
-    def step(self):
-        self.__pipe.send([self.STEP, None])
-        return self.__pipe.recv()
+  def _set_up(self):
+    pass
 
-    def close(self):
-        self.__pipe.send([self.EXIT, None])
-        self.__pipe.close()
+  def step(self):
+    self.__pipe.send([self.STEP, None])
+    return self.__pipe.recv()
 
-    def set_network(self, net):
-        if not self.config.async_actor:
-            self._network = net
-        else:
-            self.__pipe.send([self.NETWORK, net])
+  def close(self):
+    self.__pipe.send([self.EXIT, None])
+    self.__pipe.close()
+
+  def set_network(self, net):
+    if not self.config.async_actor:
+      self._network = net
+    else:
+      self.__pipe.send([self.NETWORK, net])
